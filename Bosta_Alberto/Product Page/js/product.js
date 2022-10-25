914fa005d8507190b5015974b11cf72edbff1b62
Bosta <116528076+BostaTech@users.noreply.github.com>
2022-10-25 14:49:11 +0100

    Delete Bosta_Alberto directory

--- Bosta_Alberto/Product Page/js/product.js
+++
@@ -1,8 +0,0 @@
-// add to cart script
-// var cartcount = document.getElementById("cart-counter");
-var currentcount = 0;
-function count1 (){
-    currentcount++;
-    document.getElementById("cart-counter").innerHTML = currentcount;
-}
-
